d6c553e8e56eac233d4c1c927740f0949776d20d
brandonweis <kokweiong@gmail.com>
2015-11-30 16:39:55 -0800

    commit for reference

---
+++ README.md
@@ -0,0 +1,6 @@
+1. run "python main"
+2. Send GET request to "http://127.0.0.1:5000/api/v1/extract?url=<URL>"
+
+or
+
+1. run "python summary <URL>"
---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template,jsonify
+from goose import Goose
+from summary import Summary # from filename import Classname
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/extract')
+def extract():
+    url = request.args.get('url')
+    g = Goose()
+    article = g.extract(url=url)
+    print article
+    # response = {'title' : article.title , 'text' : article.cleaned_text}
+    # return jsonify(response)
+    summarised_article = Summary({'title' : article.title , 'content' : article.cleaned_text})
+    summarised_article.print_result()
+    return jsonify(summarised_article.get_result())
+
+if __name__ == "__main__":
+    app.run(debug=True)
